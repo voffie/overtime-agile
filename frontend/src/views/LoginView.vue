e6504763eba0ce6069d60f3166ed4925bb61bb64
efpcode <efpjobhunt@gmail.com>
2025-10-16 19:34:26 +0200

    (adds) validation on for frontend and form fields warnings

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,33 +1,111 @@
 <script setup>
+import { useRouter } from 'vue-router'
 import { ref, reactive } from "vue"
 import Button from "@/components/Button.vue"
 
 const isLogin = ref(true)
+const routeToNextPage = useRouter();
 
 const formValues = reactive({
+  username: "",
+  password: "",
+  repeatPassword: "",
+})
+
+const errorMessages = reactive({
   username: "",
   password: "",
   repeatPassword: ""
 })
 
-const test = () => {
-  console.log("working")
-  console.log(formValues.username)
-  isValidUsername()
+const formValidator = (mode = "login") => {
+  const usernameValidator = isValidUsername(formValues.username)
+  const passwordValidator = isValidPasswordFormat(formValues.password)
+  const passwordsValueValidator = isTheSamePassword(formValues.password, formValues.repeatPassword)
+  let validForm = true;
+  errorMessages.username = "";
+  errorMessages.password = "";
+  errorMessages.repeatPassword = "";
+
+  if (!usernameValidator) {
+    errorMessages.username = ">>> Username requires: 5+ characters. Allowed:  A-Za-z, 0-9, space, and . <<<"
+    validForm = false;
+  }
+
+  if (!passwordValidator) {
+    errorMessages.password = ">>> Password requires: 8+ characters. Allowed:  A-Za-z, 0-9, symbols:  @ ! ? . and space. <<<"
+    validForm = false;
+  }
+
+  if (mode === "register") {
+
+    if (!passwordsValueValidator) {
+      errorMessages.repeatPassword = ">>> Password entered are not matching <<<"
+      validForm = false;
+    }
+
+  }
+
+  return validForm;
+
 
 }
-const registrationForm = () => {
-  console.log("Navigates to other page")
+
+const handlesLogin = () => {
+  if (formValidator("login")) {
+    routeToNextPage.push({ name: "game-intro" })
+
+  }
+
+}
+
+const handlesRegistration = () => {
+  if (formValidator("register")) {
+    console.log("new register player")
+
+  }
+
+}
+
+const changeForm = () => {
+  console.log("Loads page")
   isLogin.value = !isLogin.value
 
 }
 
 const isValidUsername = (userNameEntered) => {
+  if (!userNameEntered) {
+    return false;
+  }
+
+  // Accepts letters, digits, nordic åäö
   const patternRegex = /^[A-Za-zÅÄÖåäö0-9. ]+$/;
   const miniLength = 5;
   return patternRegex.test(userNameEntered) && userNameEntered.length >= miniLength;
 
 }
+
+const isValidPasswordFormat = (userPasswordEntered) => {
+
+  if (!userPasswordEntered) {
+    return false;
+  }
+
+  // Accepts letters, digits symbols: @?-._<space>
+  const patternRegex = /^[A-Za-z0-9@!?_.\- ]{8,64}$/;
+  const miniLength = 8;
+  return patternRegex.test(userPasswordEntered) && userPasswordEntered.length >= miniLength;
+}
+
+const isTheSamePassword = (userPasswordEntered, repeatedEnteredPassword) => {
+  if (!userPasswordEntered || !repeatedEnteredPassword) {
+    return false;
+  }
+  return userPasswordEntered === repeatedEnteredPassword;
+
+}
+
+
 </script>
 
 <template>
@@ -35,7 +113,7 @@ const isValidUsername = (userNameEntered) => {
     <h1 class="login-header">{{ isLogin ? "Overtime" : "Credentials" }}</h1>
     <section v-if="isLogin" class="login-page">
       <section class="form-container">
-        <form class="form-login" @submit.prevent="test">
+        <form class="form-login" @submit.prevent="handlesLogin">
           <fieldset class="form-fieldset">
             <legend class="form-legend">Welcome</legend>
             <div class="form-field">
@@ -44,16 +122,18 @@ const isValidUsername = (userNameEntered) => {
               <input v-model="formValues.username" class="form-input" type="text" id="username" name="username"
                 placeholder="Enter username" required>
             </div>
+            <p class="warning-message" v-if="errorMessages.username"> <code>{{ errorMessages.username }}</code></p>
             <div class="form-field">
               <label class="form-label" for="password"><img src="@/assets/img/keyIcon.svg" alt="key icon"></label>
-              <input class="form-input" type="text" id="password" name="password" placeholder="Enter password" required>
+              <input v-model="formValues.password" class="form-input" type="password" id="text" name="password"
+                placeholder="Enter password" required>
             </div>
+            <p class="warning-message" v-if="errorMessages.password"> <code> {{ errorMessages.password }} </code></p>
           </fieldset>
           <div class="form-buttons">
             <Button class="form-button" text="LOG IN" type="submit" />
             <div class="secondary-form-container">
-              <Button class=" form-button form-button-secondary" text="CREATE ACCOUNT"
-                @click.prevent="registrationForm" />
+              <Button class=" form-button form-button-secondary" text="CREATE ACCOUNT" @click.prevent="changeForm" />
             </div>
           </div>
         </form>
@@ -62,30 +142,35 @@ const isValidUsername = (userNameEntered) => {
 
     <section v-else class="registration-page">
       <section class="registration-container">
-        <form class="form-registration" @submit.prevent="test">
+        <form class="form-registration" @submit.prevent="handlesRegistration">
           <fieldset class="registration-fieldset">
             <legend class="form-legend">Enter Your Credentials</legend>
             <div class="form-field">
               <label class="form-label" for="username"><img src="@/assets/img/usernameIcon.svg"
                   alt="username icon"></label>
-              <input class="form-input" type="text" id="username" name="username" placeholder="Enter username" required>
+              <input v-model="formValues.username" class="form-input" type="text" id="username" name="username"
+                placeholder="Enter username" required>
             </div>
+            <p class="warning-message" v-if="errorMessages.username"> <code>{{ errorMessages.username }}</code></p>
             <div class="form-field">
               <label class="form-label" for="password"><img src="@/assets/img/keyIcon.svg" alt="key icon"></label>
-              <input class="form-input" type="text" id="password" name="password" placeholder="Enter password" required>
+              <input v-model="formValues.password" class="form-input" type="text" id="password" name="password"
+                placeholder="Enter password" required>
             </div>
+            <p class="warning-message" v-if="errorMessages.password"> <code> {{ errorMessages.password }} </code></p>
             <div class="form-field">
               <label class="form-label" for="password"><img src="@/assets/img/keyIconRepeat.svg"
                   alt="two arrows icon circular pattern"></label>
-              <input class="form-input" type="text" id="password" name="password" placeholder="Repeat password"
-                required>
+              <input v-model="formValues.repeatPassword" class="form-input" type="text" id="password" name="password"
+                placeholder="Repeat password" required>
             </div>
+            <p class="warning-message" v-if="errorMessages.repeatPassword"> <code> {{
+              errorMessages.repeatPassword }} </code></p>
           </fieldset>
           <div class="form-buttons">
             <Button class="form-button" text="CREATE ACCOUNT" type="submit" />
             <div class="secondary-form-container">
-              <Button class="form-button form-button-secondary" text="GO BACK TO LOG IN"
-                @click.prevent="registrationForm" />
+              <Button class="form-button form-button-secondary" text="GO BACK TO LOG IN" @click.prevent="changeForm" />
             </div>
           </div>
         </form>
@@ -161,6 +246,15 @@ const isValidUsername = (userNameEntered) => {
   outline: none;
   caret-color: #000000;
   font-weight: bolder;
+}
+
+.warning-message {
+  text-align: center;
+  font-size: large;
+  text-wrap: balance;
+  color: var(--warning);
+  margin: 0.4rem auto;
+  width: 80%;
 }
 
 .form-buttons {
